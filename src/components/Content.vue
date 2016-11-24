3d781ab7174a24ff177ecdfab93b7a44d5675cb4
yanxiaodi <929213769@qq.com>
2016-11-24 21:34:08 +0800

    准备添加'关于我们'内容

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,17 +1,33 @@
 <template>
     <div id="content">
-        <Title title="热门办公室推荐" />
+        <Title :title=titleArr[0].title /><!-- 父组件向子组件传递数据 -->
         <BuildingList />
-        <Title title="关于我们" />
+        <Title :title=titleArr[1].title /><!-- 父组件向子组件传递数据 -->
+        <div class="textBox aplusoffice">
+            <h3> A+office </h3>
+            <div class="string"></div>
+        </div>
+        <div class="textBox contact">
+            <h3> 联系方式 </h3>
+            <div class="string"></div>
+        </div>
     </div>
 </template>
 
 <script>
-import BuildingList from './BuildingList.vue'
-import Title   from './Title.vue'
+import BuildingList     from './BuildingList.vue'
+import Title            from './Title.vue'
 const components = { BuildingList,Title }
 
 export default {
+    data() {
+        return{
+            titleArr: [
+                { title: '热门办公室推荐' },
+                { title: '关于我们' }
+            ]
+        }
+    },
     components: components
 }
 </script>
@@ -23,4 +39,6 @@ export default {
     +REM(padding-right,16px)
     width: ((375px-(16px)*2))/37.5px*1rem
     +bC(#FAFAFA)
+    .string
+        +string(#8f99af)
 </style>
--- src/components/Title.vue
+++ src/components/Title.vue
@@ -18,17 +18,13 @@ export default {
     @extend %flexCenter
     text-align: center
     .string
-        display: block
-        width: 100%
-        +REM(height,1px)
-        border-top: 1px solid $title-color
-        +REM(border-bottom-width,1px)
+        +string(#8f99af)
     h2
         position: absolute
         @extend %dib
         +REM(padding-left,28px)
         +REM(padding-right,28px)
-        +REM(padding-bottom,5px)
+        +REM(padding-bottom,4px)
         color: $title-color
         +bC(#FAFAFA)
         +REM(font-size,14px)
